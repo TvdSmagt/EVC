72eb1f423e3487340269e7bf7bfbc0c2d0d7234a
EVCpi <Tvdsmagt@github.com>
2017-06-19 13:20:43 +0200

    TestDriving

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,5 @@
 git add ./CMakeLists.txt ./Makefile 
 git add ./git_push.sh ./build_and_run.sh
 git add ./Serial/. ./src/.
-git commit -m "Code Cleanup"
+git commit -m "TestDriving"
 git push origin master
