2e74c4e2906849e533b396dfb42ee52ab125d526
Tijs van der Smagt <tvdsmagt@gmail.com>
2017-06-19 13:24:41 +0200

    TestDriving

--- git_push.sh
+++ git_push.sh
@@ -1,7 +1,5 @@
 git add ./CMakeLists.txt ./Makefile 
 git add ./git_push.sh ./build_and_run.sh
-git add ./Serial/. ./src/.
-git commit -m "TestDriving"
 git add ./Serial/. ./src/. ./Arduino/.
-git commit -m "Code Cleanup"
+git commit -m "TestDriving"
 git push origin master
