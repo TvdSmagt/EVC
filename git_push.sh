a5943f7ab56f2f360d6e3715dc97dac55b039359
EVCpi <Tvdsmagt@github.com>
2017-06-28 11:53:18 +0200

    findPath remake

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,5 @@
 git add ./CMakeLists.txt ./Makefile 
 git add ./git_push.sh ./build_and_run.sh
 git add ./Serial/. ./src/. ./Arduino/.
-git commit -m "Course Testing"
+git commit -m "findPath remake"
 git push origin master
--- src/findPath.cpp
+++ src/findPath.cpp
@@ -6,12 +6,14 @@ enum DriveCommand {DRIVE_STRAIGHT=0,TURN_LEFT,TURN_RIGHT};
 float pctCropTop = 0.60; //0.1 - Previous: 0.1
 float pctCropBottom = 0.25;
 int iThresh = 80; //outside: 60
-const int maxLines = 100;
+const int maxLines = 200;
 const int curved = 1;
 const int straight = 1;
-const int compRatio = 2;
+const int compRatio = 1;
 const int SHOW_LINES = 1;
 vector<Vec4i> lines;
+int md = 12;
+int dirThresh = 5;
 
 //Functions
 int findPath(InputArray src, OutputArray dst, bool display, double dWidth, double dHeight);
@@ -65,8 +67,8 @@ int findPath(InputArray src, OutputArray dst, bool display, double dWidth, doubl
 	Images.push_back(srcc);
 	Images.push_back(thresh);
 	Images.push_back(hough);
-	Images.push_back(command);
-	Mat canvas = makeCanvas(Images, dHeight,3);
+//	Images.push_back(command);
+	Mat canvas = makeCanvas(Images, dHeight*2,3);
 	namedWindow("Canvas", WINDOW_NORMAL);
 	cv::resizeWindow("Canvas", dWidth/2, dHeight/2);
 	imshow("Canvas",canvas);
@@ -86,7 +88,7 @@ void processFrame(InputArray src,OutputArray thresh,OutputArray hough, double dW
 	threshold(gray,thresh,iThresh,255,THRESH_BINARY_INV);
 	//Canny
 	//Canny(thresh,canny,20,50,3);
-	cout << "\n Draw Houghlines";
+	//cout << "\n Draw Houghlines";
 	HoughLinesP( thresh, lines, 1, CV_PI/180, 0/compRatio, 0/compRatio, 0);//200,100,0
 	for( size_t i = 0; i < lines.size(); i++ )
 	{
@@ -184,7 +186,7 @@ int findFreeSpace(InputArray src, OutputArray dst, double dWidth, double dHeight
 	Mat temp;
 	src.copyTo(temp);
 	Rect bounds(0, 0, dWidth, dHeight);
-	for(int i = 2; i<=maxLines-2; i++) // Loop over lines
+	for(int i = 1; i<=maxLines-1; i++) // Loop over lines
 	{
 		Point cen(int(dWidth*i/maxLines),int(dHeight));
 		for (int r = 5; r <= dHeight+1; r++) //Find Line End
@@ -192,27 +194,28 @@ int findFreeSpace(InputArray src, OutputArray dst, double dWidth, double dHeight
 			Point p2(dWidth*i/maxLines,dHeight-r);
 			if (!bounds.contains(p2) || (temp.at<Vec3b>(p2).val[2] >0)){
 				if(SHOW_LINES){line(temp, cen, p2, Scalar(0,255,0), 2, 3 );}
-				max_angle.push_back(i);
+				max_angle.push_back(dWidth*i/maxLines);
 				max_range.push_back(r);
 				break;
 			}
 		}
 	}
+
 	int dHeight_New = dHeight * 0.5 / compRatio;
-	int dWidth_Start = int(dWidth/7);
-	int dWidth_Stop = int(dWidth*6/7);
+	int dWidth_Start = int(dWidth/md);
+	int dWidth_Stop = int(dWidth*(md-1)/md);
 	Point d1l(dWidth_Start,dHeight);
 	Point d1h(dWidth_Start,dHeight_New);
 	Point d2l(dWidth_Stop,dHeight);
-	Point d2h(dWidth_Stop,dHeight_new);
+	Point d2h(dWidth_Stop,dHeight_New);
 
 	if(SHOW_LINES){
-		line(temp,(dWidth_Start,dHeight),(dWidth_Start,dHeight_New),Scalar(255,0,0),2,3);
-		line(temp,(dWidth_Stop,dHeight),(dWidth_Stop,dHeight_New),Scalar(255,0,0),2,3);
-		line(temp,(dWidth_Start,dHeight),(dWidth_Stop,dHeight_New),Scalar(255,0,0),2,3);
+		line(temp,d1l,d1h,Scalar(255,0,0),2,3);
+		line(temp,d1h,d2h,Scalar(255,0,0),2,3);
+		line(temp,d2l,d2h,Scalar(255,0,0),2,3);
 	}
-	for(int j = maxLines/7; j <= maxLines * 6/7; j++){
-		if ((direction == DRIVE_STRAIGHT)&&(max_range[j] < dHeight_New)){
+	for(int j = 1; j <= maxLines-1; j++){ //int j = int((maxLines)/md); j <= int((maxLines) * (md-1)/md); j++
+		if ((max_angle[j] >= dWidth_Start) && (max_angle[j] <= dWidth_Stop) && (max_range[j] < dHeight_New)){ //(direction == DRIVE_STRAIGHT)
 			nospace = true;
 			if (j < maxLines / 2){
 				favor_right++;
@@ -221,11 +224,11 @@ int findFreeSpace(InputArray src, OutputArray dst, double dWidth, double dHeight
 			}
 		}
 	}
-	if (nospace){
+	if ((nospace)&&((favor_left > dirThresh)||(favor_right >dirThresh))){
 		if (favor_left > favor_right){
-			direction = TURN_LEFT; cout << "\tTurn Left";
-		} else {
-			direction = TURN_RIGHT;cout << "\tTurn Right";
+			direction = TURN_LEFT; cout << "\tTurn Left   " << favor_right << " " << favor_left ;
+		} else if(favor_right > favor_left){
+			direction = TURN_RIGHT;cout << "\tTurn Right  " << favor_right << " " << favor_left;
 		}
 	} else cout << "\tGo Straight " << favor_right << " " << favor_left;
 	temp.copyTo(dst);
